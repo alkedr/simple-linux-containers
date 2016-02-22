0ea6f50ad86f913601abaf1653ad93533af9cf5d
alkedr <alkedr@gmail.com>
2016-02-22 20:36:28 +0300

    test_framework.h refactoring, build in separate directory

--- Makefile
+++ Makefile
@@ -1,16 +1,24 @@
+BUILD_DIR := build
 HEADERS := hmlc.h
 SOURCES := hmlc.c
 TEST_HEADERS := test_framework.h
 TEST_SOURCES := $(wildcard test_*.c)
-TEST_EXECUTABLES := $(patsubst %.c,%,$(TEST_SOURCES))
+TEST_EXECUTABLES := $(addprefix $(BUILD_DIR)/,$(patsubst %.c,%,$(TEST_SOURCES)))
 
-run-tests: $(addprefix run-,$(TEST_EXECUTABLES)) Makefile
+test: $(BUILD_DIR) compile-tests run-tests
 
-run-test_%: test_% Makefile
-	@echo "RUN $@"
-	@rm -Rf test_dir && ./$< | sed -e 's/^/  /' && rm -Rf test_dir
+$(BUILD_DIR):
+	mkdir -p $@
 
-test_%: test_%.c $(HEADERS) $(SOURCES) $(TEST_HEADERS) Makefile
+compile-tests: $(TEST_EXECUTABLES)
+
+run-tests: $(addprefix run-,$(TEST_EXECUTABLES))
+
+run-$(BUILD_DIR)/test_%: $(BUILD_DIR)/test_%
+	@echo "RUN $<"
+	@rm -Rf $(BUILD_DIR)/test_dir && ./$< | sed -e 's/^/  /' && rm -Rf $(BUILD_DIR)/test_dir
+
+$(BUILD_DIR)/test_%: test_%.c $(HEADERS) $(SOURCES) $(TEST_HEADERS) Makefile
 	@echo "CC  $@"
 	@gcc -Wall -Wextra -Wno-unused-parameter -Wno-unused-function $< $(SOURCES) -o $@
 
--- test_framework.h
+++ test_framework.h
@@ -11,66 +11,54 @@
 #define RED "\033[0;31m"
 #define NORMAL "\033[0m"
 
-static const char * create_empty_fs_root_directory() {
-  mkdir("test_dir", S_IRWXU | S_IRWXG | S_IRWXO);
-  return "test_dir";
+
+
+#define pass(description, format, args...) {\
+  fprintf(stdout, GREEN "PASS: %s: " format NORMAL "\n", description, ##args);\
+}
+
+#define fail(description, format, args...) {\
+  fprintf(stdout, RED "FAIL: %s: " format NORMAL "\n", description, ##args);\
+  exit(1);\
+}
+
+#define check(condition, description, format, args...) {\
+  if (condition) {\
+    pass(description, format, ##args);\
+  } else {\
+    fail(description, format, ##args);\
+  }\
 }
 
 
 static void assertIntEquals(const char * description, int expected, int actual) {
-  if (expected != actual) {
-    fprintf(stdout, RED "FAIL: %s: expected %d, got %d" NORMAL "\n", description, expected, actual);
-    exit(1);
-  } else {
-    fprintf(stdout, GREEN "PASS: %s: %d" NORMAL "\n", description, actual);
-  }
+  check(expected == actual, description, "expected %d, got %d", expected, actual);
 }
 
 static void assertIntNotEquals(const char * description, int expected, int actual) {
-  if (expected == actual) {
-    fprintf(stdout, RED "FAIL: %s: expected not %d, got %d" NORMAL "\n", description, expected, actual);
-    exit(1);
-  } else {
-    fprintf(stdout, GREEN "PASS: %s: expected not %d, got %d" NORMAL "\n", description, expected, actual);
-  }
+  check(expected != actual, description, "expected not %d, got %d", expected, actual);
 }
 
 
 static void assertStrEquals(const char * description, const char * expected, const char * actual) {
-  if (strcmp(expected, actual)) {
-    fprintf(stdout, RED "FAIL: %s: expected '%s', got '%s'" NORMAL "\n", description, expected, actual);
-    exit(1);
-  } else {
-    fprintf(stdout, GREEN "PASS: %s: '%s'" NORMAL "\n", description, actual);
-  }
+  check(!strcmp(expected, actual), description, "expected '%s', got '%s'", expected, actual);
+}
+
+static void assertStrNotEquals(const char * description, const char * expected, const char * actual) {
+  check(strcmp(expected, actual), description, "expected not '%s', got '%s'", expected, actual);
 }
 
 
 static void assertGreaterThan(const char * description, int expected, int actual) {
-  if (actual > expected) {
-    fprintf(stdout, GREEN "PASS: %s: expected >%d, got %d" NORMAL "\n", description, expected, actual);
-  } else {
-    fprintf(stderr, RED "FAIL: %s: expected >%d, got %d" NORMAL "\n", description, expected, actual);
-    exit(1);
-  }
+  check(actual > expected, description, "expected >%d, got %d", expected, actual);
 }
 
 static void assertPtrEquals(const char * description, void * expected, void * actual) {
-  if (expected != actual) {
-    fprintf(stdout, RED "FAIL: %s: expected %p, got %p" NORMAL "\n", description, expected, actual);
-    exit(1);
-  } else {
-    fprintf(stdout, GREEN "PASS: %s: %p" NORMAL "\n", description, actual);
-  }
+  check(expected == actual, description, "expected %p, got %p", expected, actual);
 }
 
 static void assertPtrNotEquals(const char * description, void * expected, void * actual) {
-  if (expected == actual) {
-    fprintf(stdout, RED "FAIL: %s: expected not %p" NORMAL "\n", description, actual);
-    exit(1);
-  } else {
-    fprintf(stdout, GREEN "PASS: %s: expected not %p, got %p" NORMAL "\n", description, expected, actual);
-  }
+  check(expected != actual, description, "expected not %p, got %p", expected, actual);
 }
 
 static void assertNull(const char * description, void * actual) {
@@ -83,27 +71,26 @@ static void assertNotNull(const char * description, void * actual) {
 
 
 static void assertFileExists(const char * description, const char * file_name) {
-  if (access(file_name, F_OK) == -1) {
-    fprintf(stdout, RED "FAIL: %s: file '%s' should exist" NORMAL "\n", description, file_name);
-    exit(1);
-  } else {
-    fprintf(stdout, GREEN "PASS: %s: file '%s' should exist" NORMAL "\n", description, file_name);
-  }
+  check(access(file_name, F_OK) != -1, description, "file '%s' should exist", file_name);
 }
 
 static void assertFileDoesNotExist(const char * description, const char * file_name) {
-  if (access(file_name, F_OK) == -1) {
-    fprintf(stdout, GREEN "PASS: %s: file '%s' should not exist" NORMAL "\n", description, file_name);
-    exit(1);
-  } else {
-    fprintf(stdout, RED "FAIL: %s: file '%s' should not exist" NORMAL "\n", description, file_name);
-  }
+  check(access(file_name, F_OK) == -1, description, "file '%s' should not exist", file_name);
 }
+
+
 
 
 
 #define step(message, args...) fprintf(stdout, "LOG : " message "\n", ## args);
 
+
+
+
+static const char * create_empty_fs_root_directory() {
+  mkdir("build/test_dir", S_IRWXU | S_IRWXG | S_IRWXO);
+  return "build/test_dir";
+}
 
 
 static const char * concat_path_segments(const char * segment1, const char * segment2) {
